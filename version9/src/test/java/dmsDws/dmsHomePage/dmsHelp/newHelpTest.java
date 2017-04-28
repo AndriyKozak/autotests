39813c62acb8ac6e4c44ffb387bd8cff47b16c86
Diana <dianalebiga@gmail.com>
2017-04-28 22:18:48 +0300

    New test file for Andrij from Diana

---
+++ version9/src/test/java/dmsDws/dmsHomePage/dmsHelp/newHelpTest.java
@@ -0,0 +1,11 @@
+package dmsDws.dmsHomePage.dmsHelp;
+
+/**
+ * Created by Diana on 28.04.2017.
+ */
+public class newHelpTest {
+
+    public static void main(String[] args) {
+        System.out.println("This is Diana!");
+    }
+}
