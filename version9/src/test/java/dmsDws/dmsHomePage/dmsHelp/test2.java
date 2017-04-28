6cc04b584dce1c2d9baa1afe7dd62461e2ea419b
Diana <dianalebiga@gmail.com>
2017-04-28 22:25:29 +0300

    Test 2

---
+++ version9/src/test/java/dmsDws/dmsHomePage/dmsHelp/test2.java
@@ -0,0 +1,10 @@
+package dmsDws.dmsHomePage.dmsHelp;
+
+/**
+ * Created by Diana on 28.04.2017.
+ */
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println("ha ha ha");
+    }
+}
